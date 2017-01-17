a6c226e6e0bad301fc8522b5809727c24d75a00b
James Kelley <jkelley3@gmail.com>
2017-01-16 20:37:26 -0500

    [Keys] Add email to the unique writeboard key table

--- tables/surface_keys.sql
+++ tables/surface_keys.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[surface_keys]
 (
 	[KeyID] BIGINT NOT NULL PRIMARY KEY, 
-    [KeyValue] VARCHAR(50) NOT NULL
+    [KeyValue] VARCHAR(50) NOT NULL, 
+    [KeyEmail] VARCHAR(50) NOT NULL 
 )
