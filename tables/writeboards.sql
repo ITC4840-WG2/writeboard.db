deecfb5ff366ab68d7f1d42b605535be07603237
James Kelley <jkelley3@gmail.com>
2017-01-23 20:23:29 -0500

    [Registration] Initial WriteBoard registration table design. WRIT-14

---
+++ tables/writeboards.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[writeboards]
+(
+	[WriteBoardID] BIGINT NOT NULL PRIMARY KEY, 
+    [WriteBoardKey] VARCHAR(50) NOT NULL, 
+    [WriteBoardEmail] VARCHAR(50) NOT NULL, 
+    [WriteBoardDescription] VARCHAR(3000) NULL 
+)
