c934dcb7fe07f6a0c710fc3be434e5659b810b1d
kelleyj <jkelley3@gmail.com>
2017-04-25 21:09:40 -0400

    Make writeboardkey field a PK

--- tables/writeboards.sql
+++ tables/writeboards.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[writeboards]
 (
-	[WriteBoardID] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+	[WriteBoardID] BIGINT NOT NULL  IDENTITY, 
     [WriteBoardKey] VARCHAR(50) NOT NULL, 
     [WriteBoardEmail] VARCHAR(50) NOT NULL, 
     [WriteBoardDescription] VARCHAR(3000) NULL, 
     [WriteBoardState] VARCHAR(MAX) NULL, 
     [WriteBoardWidth] INT NOT NULL DEFAULT 1920, 
-    [WriteboardHeight] INT NOT NULL DEFAULT 1080 
+    [WriteboardHeight] INT NOT NULL DEFAULT 1080, 
+    PRIMARY KEY ([WriteBoardID], [WriteBoardKey]) 
 )
